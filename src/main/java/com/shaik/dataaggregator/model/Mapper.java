e30d8d85624ee37510bb937b4d72c269e7cf15b2
SHAISRIN000 <sk.srinivas@gmail.com>
2018-04-20 13:18:54 -0500

    Update Mapper.java

--- src/main/java/com/shaik/dataaggregator/model/Mapper.java
+++ src/main/java/com/shaik/dataaggregator/model/Mapper.java
@@ -1,9 +1,15 @@
 package com.shaik.dataaggregator.model;
 
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
+
+import javax.lang.model.util.Elements;
+
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import org.codehaus.jackson.JsonFactory;
 import org.codehaus.jackson.JsonNode;
@@ -16,35 +22,57 @@ import org.codehaus.jackson.node.ObjectNode;
 
 public class Mapper {
 
-	public static PropertyData translate(String request,Report report) throws JsonProcessingException, IOException   {
+	public static Report translate(String request) throws JsonProcessingException, IOException   {
 			JsonParser parser = new JsonFactory().createJsonParser(request);
 		   JsonNode rootNode = new ObjectMapper().readTree(parser);
 	      String source="PropertyVision";
 	       ObjectNode currentNode;
 	       currentNode =(ObjectNode) rootNode;
+	        Report r=new Report();
+	        r.setCreateTS( new Timestamp(System.currentTimeMillis()).toString());
+	        r.setId(UUID.randomUUID().toString());
+	      Set<String>  srcs= new HashSet<>();
+	      srcs.add(source);
+	        r.setSources(srcs);
+	        r.setStatus("InProgress");
 	        
-	       
+	        List<ElementInference> elemnts=new ArrayList<ElementInference>();
+	        
 	       
 	        JsonNode numberStoriesInferenceNode=rootNode.get("numberStoriesInference");
 	        
 	    	ElementInference numberofstories=new ElementInference("numberOfStories",numberStoriesInferenceNode.get("numberStories").asText(),numberStoriesInferenceNode.get("inputUrl").asText(),numberStoriesInferenceNode.get("confidence").asText(),numberStoriesInferenceNode.get("modelVersion").asText(),source);
-		
+	    	elemnts.add(numberofstories);
 	        JsonNode numberofChimneysInferenceNode=rootNode.get("numberChimneysInference");
-		    ElementInference numberofChimneys=new ElementInference("num_chimneys",numberStoriesInferenceNode.get("num_chimneys")!=null?numberStoriesInferenceNode.get("num_chimneys").asText():"0",numberStoriesInferenceNode.get("inputUrl").asText(),numberStoriesInferenceNode.get("confidence")!=null?numberStoriesInferenceNode.get("confidence").asText():"0",numberStoriesInferenceNode.get("modelVersion").asText(),source););
-
+		    ElementInference numberofChimneys=new ElementInference("num_chimneys",numberofChimneysInferenceNode.get("num_chimneys")!=null?numberofChimneysInferenceNode.get("num_chimneys").asText():"0",numberofChimneysInferenceNode.get("inputUrl").asText(),numberofChimneysInferenceNode.get("confidence")!=null?numberofChimneysInferenceNode.get("confidence").asText():"0",numberofChimneysInferenceNode.get("modelVersion").asText(),source);
+		    elemnts.add(numberofChimneys);
 		    JsonNode poolsInferenceNode=rootNode.get("poolsInference");
 		    ElementInference poolss=new ElementInference("pools",poolsInferenceNode.get("pools")!=null?poolsInferenceNode.get("pools").asText():"0",poolsInferenceNode.get("inputUrl").asText(),poolsInferenceNode.get("confidence")!=null?poolsInferenceNode.get("confidence").asText():"0",poolsInferenceNode.get("modelVersion").asText(),source);
-
+		    elemnts.add(poolss);
 		    
-	       PropertyData data=new PropertyData();
-	       List<ElementInference> elements=new ArrayList<>();
-	       elements.add(fetchNumberOfStories(response));
-	       elements.add(fetchNumberOfChimneys(response));
-	       data.setElements(elements);
-	       return data;
+		    JsonNode roofsInferenceNode=rootNode.get("roofsInference");
+		    ElementInference roofs=new ElementInference();
+		    roofs.setKey("roofs");
+		    JsonNode roofsArrayNode=roofsInferenceNode.get("roofs");
+		    if (roofsArrayNode.isArray()) {
+		        for (final JsonNode objNode : roofsArrayNode) {
+		        	
+		        	SubElementInference subElementMaterial=new SubElementInference("material",objNode.get("material").asText(),objNode.get("materialInputUrl").asText(),objNode.get("materialConfidence").asText(),objNode.get("materialModelVersion").asText(),source);
+		        	SubElementInference subElementShape=new SubElementInference("shape",objNode.get("shape").asText(),objNode.get("shapeInputUrl").asText(),objNode.get("shapeConfidence").asText(),objNode.get("shapeModelVersion").asText(),source);
+		        	List<SubElementInference>  subinferences=new ArrayList<>();
+		        	subinferences.add(subElementMaterial);
+		        	subinferences.add(subElementShape);
+		        	roofs.setSubElementsInferences(subinferences);
+		             }
+		    }
+		    
+		    elemnts.add(roofs);
+		    
+		    r.setElementInferences(elemnts);
+		    return r;
 	}
 	
-
+/*
 	private static Address convertAddress(String request) throws JsonParseException, JsonMappingException, IOException {
 		System.out.println("Started convertAddress");
 
@@ -76,5 +104,5 @@ public class Mapper {
 	   ElementInference numberofChimneys;
 	   //ElementInference numberofChimneys=new ElementInference("num_chimneys",numberStoriesInferenceNode.get("num_chimneys")!=null?numberStoriesInferenceNode.get("num_chimneys").asText():"0",numberStoriesInferenceNode.get("inputUrl").asText(),numberStoriesInferenceNode.get("confidence")!=null?numberStoriesInferenceNode.get("confidence").asText():"0");
 	   return numberofChimneys=null;
-	}
+	}*/
 }
