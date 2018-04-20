7c7fc5324002b05a168c21c1d8c52d868b08b9e6
SHAISRIN000 <sk.srinivas@gmail.com>
2018-04-20 13:00:19 -0500

    Update DataAggregatorApi.java

--- src/main/java/com/shaik/dataaggregator/api/DataAggregatorApi.java
+++ src/main/java/com/shaik/dataaggregator/api/DataAggregatorApi.java
@@ -2,7 +2,6 @@ package com.shaik.dataaggregator.api;
 
 import java.io.IOException;
 
-import org.codehaus.jackson.JsonProcessingException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,12 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.shaik.dataaggregator.db.DynamoDBClient;
 import com.shaik.dataaggregator.model.Mapper;
-import com.shaik.dataaggregator.model.ModelUtil;
-import com.shaik.dataaggregator.model.PropertyData;
 import com.shaik.dataaggregator.model.Report;
 
 @RestController()
@@ -33,9 +29,10 @@ public class DataAggregatorApi {
 	  
   @RequestMapping(value = "/{id}/propertydata", method = RequestMethod.GET)
     public ResponseEntity<?> getPropertyDataById(@PathVariable("id") String id) {
+	  //http://localhost:8080/propertydataaggregator/780800ab-201d-43c3-86e4-cc9c0a960bfd/propertydata
         System.out.println("Fetching report with id {}"+id);
-        PropertyData data= DynamoDBClient.retrieveReport(id);
-         return new ResponseEntity<PropertyData>(data, HttpStatus.OK);
+        Report data= DynamoDBClient.retrieveReport(id);
+         return new ResponseEntity<Report>(data, HttpStatus.OK);
    }
   
   @RequestMapping(value = "/{id}/propertyRawData", method = RequestMethod.GET)
@@ -47,7 +44,7 @@ public class DataAggregatorApi {
    
   @RequestMapping(value = "/property", method = RequestMethod.GET)
   public ResponseEntity<?> getPropertyData(@RequestParam("address") String address,@RequestParam("consumer") String consumer,@RequestParam(name="forceOrder", required=false) boolean forceOrder,@RequestParam(name="lookupDays", required=false) String days) {
-
+//http://localhost:8080/propertydataaggregator/property?address=3142+Lindbergh+St+Madison+WI+53704&consumer=PC
 	/*
 	 * Validate the request parameters
 	 * Check if the address already exists based on the number of days ,if it is then return the report(It requires the request scan need to find dynamodb efficient way
@@ -55,49 +52,36 @@ public class DataAggregatorApi {
 	 *        
 	 */
 	 
-	  
-	  PropertyData data=new PropertyData();
-	  Report report=null;
-	
-	String reportId=DynamoDBClient.insertRequest(address,consumer);
-	
-	String rawResponse=getRestTemplate().getForObject("https://api.property.vision/property?address="+address, String.class);
-	String source="PropertyVision";
-	String sourceId=reportId+"_"+"source";
-	DynamoDBClient.insertRawReport(reportId,rawResponse,source,sourceId);
-	  report=ModelUtil.createReport();
-		  
-	/*  try {
-		  
-		  //This need to be the Report in DynamoDB
-	//	data=Mapper.translate(rawResponse, address);
-	} catch (JsonProcessingException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	} catch (IOException e1) {
-		// TODO Auto-generated catch block
-		e1.printStackTrace();
-	}*/
-	  data.setId(reportId);
-	  data.setSource(source);
-	  DynamoDBClient.createReport(report);
-	  DynamoDBClient.updateRequestStatus(reportId, "completed");
-	  
-	  //Insert AuditLog
-	  //
+	    String source = "PropertyVision";
+		Report report = null;
+		String reportId = DynamoDBClient.insertRequest(address, consumer);
+		String rawResponse = getRestTemplate().getForObject("https://api.property.vision/property?address=" + address,
+				String.class);
+		String sourceId = reportId + "_" + "source";
+		DynamoDBClient.insertRawReport(reportId, rawResponse, source, sourceId);
+		try {
+			report=Mapper.translate(rawResponse);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//report = ModelUtil.createReport();
+		DynamoDBClient.createReport(report);
+		DynamoDBClient.updateRequestStatus(reportId, "completed");
 
-	  if (data == null) {
-      	 System.out.println("DSAL report Error.");
-          return new ResponseEntity("DSAL Report No found", HttpStatus.NOT_FOUND);
-      }
-	  
-	  //ApiResponse populate;
-      return new ResponseEntity<PropertyData>(data, HttpStatus.OK);
-  }
+		/*
+		 * //Insert AuditLog //
+		 * 
+		 * if (data == null) { System.out.println("DSAL report Error."); return new
+		 * ResponseEntity("DSAL Report No found", HttpStatus.NOT_FOUND); }
+		 */
+
+		// ApiResponse populate;
+		return new ResponseEntity<Report>(report, HttpStatus.OK);
+	}
 	
   private OAuth2RestOperations getRestTemplate() {
-		long currentTS= System.currentTimeMillis();
-	    OAuth2AccessToken token;
+		 OAuth2AccessToken token;
 		if (System.currentTimeMillis()>tockenExpireTS) {
 			token=restTemplate.getAccessToken();
 			tockenExpireTS=System.currentTimeMillis()+token.getExpiresIn()*1000;
@@ -111,5 +95,6 @@ public class DataAggregatorApi {
   public ResponseEntity<?> getPing(@RequestParam(name="type",required=false) String pingType) {
 	 	String str=getRestTemplate().getForObject("https://api.property.vision/property?address=3142+Lindbergh+St+Madison+WI+53704", String.class);
 		System.out.println(str);	
+		 return new ResponseEntity<String>(str, HttpStatus.OK);
   }
 }
